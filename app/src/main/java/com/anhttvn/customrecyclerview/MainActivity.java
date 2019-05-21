49b49645cc4573f639b14c48083a3a76005b7aa9
tienanhit <giakhang22102016@gmail.com>
2019-05-21 23:53:46 +0700

    Initial commit

---
+++ app/src/main/java/com/anhttvn/customrecyclerview/MainActivity.java
@@ -0,0 +1,72 @@
+package com.anhttvn.customrecyclerview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+import com.anhttvn.customrecyclerview.adapter.ListAdapter;
+import com.anhttvn.customrecyclerview.model.ItemAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecycleview;
+    private List<ItemAdapter> mList = new ArrayList<>();
+    private ListAdapter mAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+        addList();
+        adapter();
+    }
+    private void init(){
+        mRecycleview = findViewById(R.id.listView);
+    }
+    private void addList(){
+        ItemAdapter itemAdapter = new ItemAdapter();
+        itemAdapter.setImage(R.drawable.cachua);
+        itemAdapter.setText("Tomato");
+        mList.add(itemAdapter);
+
+        itemAdapter = new ItemAdapter();
+        itemAdapter.setImage(R.drawable.bo);
+        itemAdapter.setText("butter");
+        mList.add(itemAdapter);
+
+        itemAdapter = new ItemAdapter();
+        itemAdapter.setImage(R.drawable.cam);
+        itemAdapter.setText("oranges");
+        mList.add(itemAdapter);
+
+        itemAdapter = new ItemAdapter();
+        itemAdapter.setImage(R.drawable.quaxoai);
+        itemAdapter.setText("mango");
+        mList.add(itemAdapter);
+
+        itemAdapter = new ItemAdapter();
+        itemAdapter.setImage(R.drawable.dau);
+        itemAdapter.setText("strawberry");
+
+        mList.add(itemAdapter);
+        itemAdapter = new ItemAdapter();
+        itemAdapter.setImage(R.drawable.tao);
+        itemAdapter.setText("Apple");
+        mList.add(itemAdapter);
+
+        mList.add(itemAdapter);
+        itemAdapter = new ItemAdapter();
+        itemAdapter.setImage(R.drawable.oi);
+        itemAdapter.setText("guava fruit");
+        mList.add(itemAdapter);
+    }
+
+    private void adapter(){
+        mAdapter = new ListAdapter(mList, this);
+        mRecycleview.setAdapter(mAdapter);
+        mAdapter.notifyDataSetChanged();
+    }
+}
---
+++ app/src/main/java/com/anhttvn/customrecyclerview/adapter/ListAdapter.java
@@ -0,0 +1,60 @@
+package com.anhttvn.customrecyclerview.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.anhttvn.customrecyclerview.R;
+import com.anhttvn.customrecyclerview.model.ItemAdapter;
+
+import java.util.List;
+
+public class ListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private List<ItemAdapter> mList;
+    private Context mContext;
+    public ListAdapter(List<ItemAdapter> list, Context context){
+        super();
+        mList = list;
+        mContext = context;
+    }
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_custom, parent, false);
+
+        ViewHolder viewHolder = new ViewHolder(v);
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int position) {
+        ItemAdapter itemAdapter = mList.get(position);
+        ((ViewHolder) viewHolder).mTv_name.setText(itemAdapter.getText());
+        ((ViewHolder) viewHolder).mImg.setImageResource(itemAdapter.getImage());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mList.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder{
+
+        public TextView mTv_name;
+        public ImageView mImg;
+        public ViewHolder(View itemView) {
+
+            super(itemView);
+            mTv_name = (TextView) itemView.findViewById(R.id.tv_name);
+            mImg = (ImageView) itemView.findViewById(R.id.img_item);
+
+        }
+    }
+}
